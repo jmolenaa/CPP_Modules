db71cd0d7bb13e1602ca9a12fd6c46a2647d858d
jmolenaa <jmolenaa@student.codam.nl>
2024-03-18 16:11:20 +0100

    ex00 done

---
+++ CPP_03/ex00/src/main.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jmolenaa <jmolenaa@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/03/18 10:37:35 by jmolenaa      #+#    #+#                 */
+/*   Updated: 2024/03/18 10:37:35 by jmolenaa      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+int	main(){
+	{
+		std::cout << "\nUsing up all energy\n\n";
+		ClapTrap	a;
+
+		a.attack("Chad");
+		a.attack("Chad");
+		a.attack("Chad");
+		a.attack("Chad");
+		a.attack("Chad");
+		a.attack("Chad");
+		a.attack("Chad");
+		a.attack("Chad");
+		a.attack("Chad");
+		a.attack("Chad");
+		a.attack("Chad");
+		a.beRepaired(10);
+		a.printClaptrap();
+	}
+	{
+		std::cout << "\nLosing all hp\n\n";
+		ClapTrap	a;
+
+		a.takeDamage(5);
+		a.takeDamage(10);
+		a.attack("Chad");
+		a.beRepaired(10);
+		a.printClaptrap();
+	}
+	{
+		std::cout << "\nTaking damage and repairing it\n\n";
+		ClapTrap	a;
+
+		a.printClaptrap();
+		a.takeDamage(5);
+		a.printClaptrap();
+		a.beRepaired(1);
+		a.beRepaired(1);
+		a.beRepaired(1);
+		a.printClaptrap();
+		a.beRepaired(5);
+		a.printClaptrap();
+		a.takeDamage(20);
+		a.printClaptrap();
+	}
+}
