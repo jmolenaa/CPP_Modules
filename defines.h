b0e4b0db33f1f29257c8ec18570a6ed4124ac700
jmolenaa <jmolenaa@student.codam.nl>
2024-07-19 10:26:41 +0200

    added macros for coloured string printing

--- defines.h
+++ defines.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   defines.h                                          :+:    :+:            */
+/*   defines.h                                          :+:      :+:    :+:   */
 /*                                                     +:+                    */
 /*   By: jmolenaa <jmolenaa@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/26 17:17:30 by jmolenaa      #+#    #+#                 */
-/*   Updated: 2024/03/26 17:17:30 by jmolenaa      ########   odam.nl         */
+/*   Updated: 2024/07/19 10:26:15 by jmolenaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@
 #define YELLOW "\x1b[33m"
 #define BLUE "\x1b[34m"
 #define RESET "\x1b[0m"
+
+#define REDSTRING(str) RED str RESET
+#define GREENSTRING(str) GREEN str RESET
+#define CYANSTRING(str) CYAN str RESET
+#define YELLOWSTRING(str) YELLOW str RESET
+#define BLUESTRING(str) BLUE str RESET
 
 #endif
 
