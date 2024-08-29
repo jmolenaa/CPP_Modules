201a10ce99d70379a6624d8accc93ee9bb4f1a3c
Jan Molenaar <molenaarjana@gmail.com>
2024-08-29 09:29:59 +0200

    syncing with laptop

---
+++ CPP_09/ex01/incs/RPN.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   RPN.hpp                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: janmolenaar <janmolenaar@student.codam.nl>   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/08/28 12:07:26 by janmolenaar   #+#    #+#                 */
+/*   Updated: 2024/08/28 12:07:26 by janmolenaar   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+//
+// Created by Jan Molenaar on 28/08/2024.
+//
+
+#ifndef CPP_09_RPN_HPP
+#define CPP_09_RPN_HPP
+
+
+class RPN {
+
+public:
+
+private:
+it
+};
+
+
+#endif //CPP_09_RPN_HPP
